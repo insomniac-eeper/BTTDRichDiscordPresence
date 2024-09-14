9d4af72baaece782930c4ff654e8b7a163d8d985
insomniac-eeper <171281312+insomniac-eeper@users.noreply.github.com>
2024-09-14 23:15:15 +0200

    Remove IsExternalInit since net6 has record support oob

--- src/Data/IsExternalInit.cs
+++
@@ -1,7 +0,0 @@
-﻿// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices;
-
-/// <summary>
-/// We need this for C# 9.0 records to work.
-/// </summary>
-internal static class IsExternalInit{}
